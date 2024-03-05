b9f6e14b0696c342b681861d13b48c59780df5fd
Felipe Laurindo <felipe_nu@hotmail.com>
2024-03-05 10:48:49 -0400

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,82 @@
 <template>
-  <v-layout>
-    <v-app-bar color="indigo-darken-3">
-      <v-app-bar-title class="flex text-center">
-        Conversor de taxas de juros
-      </v-app-bar-title>
-    </v-app-bar>
+  <v-container fluid>
+    <v-layout>
+      <v-app-bar color="indigo-darken-3">
+        <v-app-bar-title class="flex text-center">
+          Conversor de taxas de juros
+        </v-app-bar-title>
+      </v-app-bar>
 
-    <v-main>
-      <v-row justify="center" class="mt-3">
-        <v-col align="center">
-          <v-card
-            color="indigo-darken-1"
-            variant="outlined"
-            max-width="1000"
-            class="mx-2 py-1"
-          >
-            <v-card-title class="mb-2 text-wrap">
-              <p> Conversor de taxas de juros </p>
-              <p> Anual ↔ Mensal </p>
-            </v-card-title>
+      <v-main>
+        <v-row justify="center" class="mt-3">
+          <v-col align="center" class="pa-0">
+            <v-card
+              color="indigo-darken-1"
+              variant="outlined"
+              max-width="1000"
+              class="mx-2 py-1"
+            >
+              <v-card-title class="mb-2 text-wrap">
+                <p> Conversor de taxas de juros </p>
+                <p> Anual ↔ Mensal </p>
+              </v-card-title>
 
-            <v-card-subtitle class="mb-2 text-wrap">
-              Escolha qual conversão deseja realizar
-            </v-card-subtitle>
+              <v-card-subtitle class="mb-2 text-wrap">
+                Escolha qual conversão deseja realizar
+              </v-card-subtitle>
 
-            <v-card-item>
-              <v-radio-group inline v-model="radios">
-                <v-radio label="Anual → Mensal" value="true"></v-radio>
-                <v-radio label="Mensal → Anual" value="false"></v-radio>
-              </v-radio-group>
+              <v-card-item>
+                <v-radio-group inline v-model="radios">
+                  <v-radio label="Anual → Mensal" value="true"></v-radio>
+                  <v-radio label="Mensal → Anual" value="false"></v-radio>
+                </v-radio-group>
 
-              <v-row justify="center">
-                <v-col cols="11" sm="6" align="center">
-                  <v-text-field
-                    hide-details
-                    label="Valor da taxa de juros (%)"
-                    type="number"
-                    v-model="fee"
-                    :suffix="radios == 'true' ? '% a.a.' : '% a.m.'"
-                    variant="outlined"
-                  >
-                  </v-text-field>
-                </v-col>
-              </v-row>
+                <v-row justify="center">
+                  <v-col cols="11" sm="6" align="center">
+                    <v-text-field
+                      hide-details
+                      label="Valor da taxa de juros (%)"
+                      type="number"
+                      v-model="fee"
+                      :suffix="radios == 'true' ? '% a.a.' : '% a.m.'"
+                      variant="outlined"
+                    >
+                    </v-text-field>
+                  </v-col>
+                </v-row>
 
-              <v-row justify="center">
-                <v-col align="center">
-                  <v-btn icon="mdi-calculator" @click="calculate">
-                  </v-btn>
-                </v-col>
-              </v-row>
+                <v-row justify="center">
+                  <v-col align="center">
+                    <v-btn icon="mdi-calculator" @click="calculate">
+                    </v-btn>
+                  </v-col>
+                </v-row>
 
-              <v-row justify="center">
-                <v-col cols="11" sm="6" align="center">
-                  <v-text-field
-                    color="blue"
-                    focused
-                    readonly
-                    v-model="feeCalculated"
-                    :suffix="radios == 'true' ? '% a.m.' : '% a.a.'"
-                    variant="outlined"
-                  >
-                  </v-text-field>
-                </v-col>
-              </v-row>
-            </v-card-item>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-bottom-navigation :elevation="0" mode="shift">
-        {{this.date.getDate()}} / {{this.date.getMonth()+1}} / {{this.date.getYear()+1900}}
-      </v-bottom-navigation>
-    </v-main>
-  </v-layout>
+                <v-row justify="center">
+                  <v-col cols="11" sm="6" align="center">
+                    <v-text-field
+                      color="blue"
+                      focused
+                      readonly
+                      v-model="feeCalculated"
+                      :suffix="radios == 'true' ? '% a.m.' : '% a.a.'"
+                      variant="outlined"
+                    >
+                    </v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card-item>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-main>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    date: new Date(),
     fee: "",
     feeCalculated: "0",
     radios: "true",
